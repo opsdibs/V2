2f3cdb7ad28814e2585f4ca23ad87911998ec4e8
kori5181 <stevekoruthu@gmail.com>
2025-12-06 12:19:05 +0530

    TOKEN FIX (6TH DEC)

--- src/lib/settings.js
+++ src/lib/settings.js
@@ -1,6 +1,2 @@
 export const AGORA_APP_ID = "4ac521d1134d465b80122c12340392ee"; 
-<<<<<<< HEAD
-export const AGORA_TOKEN = "007eJxTYNDdleOyvUOrebrvhwcqP/k4Iuf27m/euv3eFRUb1y2/dP4rMJgkJpsaGaYYGhqbpJiYmSZZGBgaGSUbGhmbGBhbGqWmTmswymwIZGRIrvZjYIRCEJ+fIcQjyNMtJMDf2dEnMiDYlYEBANCkIgU="; 
-=======
 export const AGORA_TOKEN = "007eJxTYGBN3507IfjMNol8LYcft1f71+fnBRz7WTzR6fIB0QL/FRkKDCaJyaZGhimGhsYmKSZmpkkWBoZGRsmGRsYmBsaWRqmphQeNMxsCGRmE+TiYGBkgEMRnZXAMdQ4xYmAAACrFHQk="; 
->>>>>>> ea0ad059f2d390806424a17e3fe45dd70ad25bae
